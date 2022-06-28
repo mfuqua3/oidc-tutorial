1b1ae9ca5f90fd71d07f0302b5ea397917fbee2d
Matt Fuqua <matt.fuqua@fortyau.com>
2022-06-28 17:25:23 -0500

    angular app architecture wireframe

--- demo2-spacetaco-oidc/tacotime-angular-app/src/app/app.module.ts
+++ demo2-spacetaco-oidc/tacotime-angular-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ClarityModule} from "@clr/angular";
+import {AuthModule} from "./auth/auth.module";
+import {TacoTimeModule} from "./tacotime/taco-time.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    AuthModule,
+    TacoTimeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ demo2-spacetaco-oidc/tacotime-angular-app/src/app/models/taco.ts
@@ -0,0 +1,6 @@
+export interface Taco {
+    awardedOn: Date;
+    givenById: string;
+    id: number;
+    ownerId: string;
+}
