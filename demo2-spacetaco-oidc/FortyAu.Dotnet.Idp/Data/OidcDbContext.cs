90df8741cc5f6bb4f23ac832637aeff454b2209e
Matt Fuqua <matt.fuqua@fortyau.com>
2022-06-29 14:12:22 -0500

    dotnet file shuffle and rename, added docker files init

---
+++ demo2-spacetaco-oidc/FortyAu.Dotnet.Idp/Data/OidcDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet.Idp.Data;
+
+public class OidcDbContext : IdentityDbContext<OidcUser>
+{
+    public OidcDbContext(DbContextOptions<OidcDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("identity");
+        base.OnModelCreating(builder);
+    }
+}
